66fcade74c6ba134518519dcd20e72e594a25be9
Kim MJ <62365297+MIN-04@users.noreply.github.com>
2020-09-02 17:53:01 +0900

    20.09.02 [단계-입출력과 연산] 2557. Hello World

---
+++ Baekjoon/Step/입출력과 연산/No2557.java
@@ -0,0 +1,13 @@
+package step.step01.input;
+
+public class No2557 {
+	/**
+	 * 단계 1 - 입출력과 연산
+	 * 2557. Hello World
+	 */
+	
+	public static void main(String[] args) {
+		System.out.print("Hello World!");
+		
+	}
+}
