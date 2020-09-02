075a65258afc036e4bade820646fb612196d2fb2
Kim MJ <62365297+MIN-04@users.noreply.github.com>
2020-09-02 17:55:28 +0900

    20.09.02 [단계-입출력과 연산] 10718. We love kriii

---
+++ Baekjoon/Step/입출력과 연산/No10718.java
@@ -0,0 +1,15 @@
+package step.step01.input;
+
+public class No10718 {
+	/**
+	 * 단계 1 - 입출력과 연산
+	 * 10718. We love kriii
+	 */
+	
+	public static void main(String[] args) {
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+	}
+	
+
+}
